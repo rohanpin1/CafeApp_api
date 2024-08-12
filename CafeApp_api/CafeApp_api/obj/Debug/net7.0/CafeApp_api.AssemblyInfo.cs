dc45475747e2d80ac987d1b330b1048d84e1f8fd
rohan kumawat <rohankumawat.pinkcity@gmail.com>
2024-08-12 12:31:38 +0530

    upload

--- CafeApp_api/CafeApp_api/obj/Debug/net7.0/CafeApp_api.AssemblyInfo.cs
+++ CafeApp_api/CafeApp_api/obj/Debug/net7.0/CafeApp_api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CafeApp_api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b8b8b5d928f8a36c2006070a0cd012a4d51a81e")]
 [assembly: System.Reflection.AssemblyProductAttribute("CafeApp_api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CafeApp_api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
