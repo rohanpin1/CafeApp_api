b9f4369d50122e0a84995cb2373b43d8f91b7003
rohan kumawat <rohankumawat.pinkcity@gmail.com>
2024-08-12 16:31:00 +0530

    Cache Removed

---
+++ CafeApp_api/CafeApp_api/Service/CommonServices.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
+namespace CafeApp_api.Service
+{
+    public static class CommonServices
+    {
+        public static SqlCommand RunCommand(string query,SqlConnection conn)
+        {
+            var cmd = new SqlCommand()
+            {
+                CommandText = query,
+                CommandType = System.Data.CommandType.Text,
+                Connection = conn
+            };
+            return cmd;
+        }
+    }
+}
